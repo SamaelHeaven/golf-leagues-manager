b283e48a8f13178aecb2af85033b0ce9c674fee8
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-02-06 15:26:47 -0500

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-const daisyui = require("daisyui");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
         extend: {}
     },
-    plugins: [daisyui]
+    plugins: [require("daisyui")]
 };
