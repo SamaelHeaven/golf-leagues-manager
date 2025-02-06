cd42247daf7d6214c9ef8aef23c4b3a91f597ab2
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-02-06 16:31:27 -0500

    Fix: SafeArea

--- src/application/main.tsx
+++ src/application/main.tsx
@@ -1,6 +1,16 @@
+import { SafeArea } from "capacitor-plugin-safe-area";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<h1>Hello, World</h1>);
+(async () => {
+    await SafeArea.addListener("safeAreaChanged", data => {
+        const { insets } = data;
+        for (const [key, value] of Object.entries(insets)) {
+            document.documentElement.style.setProperty(`--safe-area-inset-${key}`, `${value}px`);
+        }
+    });
+
+    const root = createRoot(document.getElementById("root")!);
+    root.render(<h1>Hello, World</h1>);
+})();
